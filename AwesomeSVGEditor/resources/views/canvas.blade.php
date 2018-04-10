96c94ced379eae84e89f6d3a8547ce4eed772787
Sergiy Goloviatinski <sergiy.goloviatinski@he-arc.ch>
2018-04-10 14:22:49 +0200

    the explore page is less ugly now

--- AwesomeSVGEditor/resources/views/canvas.blade.php
+++ AwesomeSVGEditor/resources/views/canvas.blade.php
@@ -6,7 +6,7 @@
       @foreach($canvas as $key => $value)
         <div class="col-md-6 col-sm-12 centered">
             <a href="canvas/{{ $value->id }}/edit"><h2>{{ $value->name }}</h2></a>
-            <svg width="250px" height="250px" viewBox="0 0 1000 1000">{!! $value->code !!}</svg>
+            <svg style="border-style: solid;" width="50%" height="50%" viewBox="0 0 1000 1000">{!! $value->code !!}</svg>
         </div>
     @endforeach
     </div>
