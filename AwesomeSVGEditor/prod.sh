f8a4da087a8928b61f05110cc976951e824a228d
Bastien Wermeille <bastien.wermeille@he-arc.ch>
2018-05-07 11:21:56 +0200

    improve prod script

--- AwesomeSVGEditor/prod.sh
+++ AwesomeSVGEditor/prod.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git pull
 sed -i 's/APP_ENV=.*/APP_ENV=production/' .env
 sed -i 's/APP_DEBUG=.*/APP_DEBUG=false/' .env
 
@@ -7,3 +8,9 @@ npm run production
 composer install --optimize-autoloader
 php artisan config:cache
 
+@echo off
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e "${GREEN}\n\t------------------------------"
+echo -e "\t| Production ran successfully |"
+echo -e "\t------------------------------${NC}"
