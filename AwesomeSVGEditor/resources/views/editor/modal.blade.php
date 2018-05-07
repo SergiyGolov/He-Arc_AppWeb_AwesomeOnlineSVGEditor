803fd86664551e3140a975394d06f0cbbe4497a3
Bastien Wermeille <bastien.wermeille@he-arc.ch>
2018-05-07 13:33:05 +0200

    Modifications on wrong branch

--- AwesomeSVGEditor/resources/views/editor/modal.blade.php
+++ AwesomeSVGEditor/resources/views/editor/modal.blade.php
@@ -1,5 +1,4 @@
 
-<!-- Small modal -->
 <div id="modal-title" class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
@@ -10,6 +9,7 @@
         </button>
       </div>
       <div class="modal-body">
+        <p>Enter a new name for your amazing image.</p>
         <input id="name-modal" class="form-control" type="text" placeholder="Default input" value="{{ isset($canvas)?$canvas->name:'' }}">
       </div>
       <div class="modal-footer">
@@ -20,38 +20,42 @@
   </div>
 </div>
 
-<!-- Large modal -->
 <div id="modal-import" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">How do you want to import the svg file?</h5>
+        <h5 class="modal-title">Import</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
+      <div class="modal-body">
+        <p>How do you want to import your image?</p>
+      </div>
       <div class="modal-footer">
-        <button id="import-new" type="button" class="btn btn-primary">Open file in new tab</button>
-        <button id="import-saveopen" type="button" class="btn btn-primary">Save current canvas and open file here</button>
-        <button id="import-discardopen" type="button" class="btn btn-primary">Discard changes and open file here</button>
+        <button id="import-new" type="button" class="btn btn-primary">Open in a new tab</button>
+        <button id="import-saveopen" type="button" class="btn btn-secondary">Save changes & Open here</button>
+        <button id="import-discardopen" type="button" class="btn btn-warning">Discard changes & Open here</button>
       </div>
     </div>
   </div>
 </div>
 
-<!-- Large modal -->
 <div id="modal-export" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-sm">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Export as</h5>
+        <h5 class="modal-title">Export</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
+      <div class="modal-body">
+        <p>Export your image as :</p>
+      </div>
       <div class="modal-footer">
-        <a id="svg-link" href="/canvas/{{ isset($canvas)?$canvas->id:'' }}/svg" role="button" class="btn btn-primary" download>SVG</a>
-        <a id="png-link" href="/canvas/{{ isset($canvas)?$canvas->id:'' }}/png" role="button" class="btn btn-primary" download>PNG</a>
+        <a id="png-link" href="/canvas/{{ isset($canvas)?$canvas->id:'' }}/png" role="button" class="btn btn-primary png-file" download></a>
+        <a id="svg-link" href="/canvas/{{ isset($canvas)?$canvas->id:'' }}/svg" role="button" class="btn btn-dark svg-file" download></a>
       </div>
     </div>
   </div>
