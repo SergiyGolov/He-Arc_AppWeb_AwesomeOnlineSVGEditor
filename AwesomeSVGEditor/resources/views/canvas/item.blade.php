29a4156fc95da393fe5318934dcd6cae04d6914f
Bastien Wermeille <bastien.wermeille@he-arc.ch>
2018-05-31 14:10:30 +0200

    First design for single svg visualisation

--- AwesomeSVGEditor/resources/views/canvas/item.blade.php
+++ AwesomeSVGEditor/resources/views/canvas/item.blade.php
@@ -3,10 +3,14 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-      <div class="col-md-6 col-sm-12">
-        <div class="centered card">
-          <h2>{{ $canvas->name }}</h2>
-          {!! $canvas->code !!}
+      <div class="col-md-8 col-sm-12 mx-auto">
+        <div class="card">
+          <div class="card-header">
+            <h3>{{ $canvas->name }}</h3><p>By <em>Author</em></p>
+          </div>
+          <div class="centered">
+            {!! $canvas->code !!}
+          <div>
         </div>
       </div>
     </div>
