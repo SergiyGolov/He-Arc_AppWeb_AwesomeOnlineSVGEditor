bed6df752d2d720303f657151db335bb5c36cf1a
Bastien Wermeille <bastien.wermeille@he-arc.ch>
2018-06-08 13:52:21 +0200

    Start help section

--- AwesomeSVGEditor/resources/views/canvas/partial/modal.blade.php
+++ AwesomeSVGEditor/resources/views/canvas/partial/modal.blade.php
@@ -250,7 +250,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">Help</h5>
@@ -259,11 +259,72 @@
         </button>
       </div>
       <div class="modal-body">
-        ...
+        <h4>Interface</h4>
+        <p>The interface is composed of 4 sections which are the followings:</p>
+        <ul>
+          <li><a href="#menu-bar">Menu bar</a></li>
+          <li><a href="#drawing-tools">Drawing tools</a></li>
+          <li><a href="#editor">Editor</a></li>
+          <li><a href="#shapes">Shapes option</a></li>
+        </ul>
+        <h4 name="menu-bar">Menu bar</h4>
+        <h5>File</h5>
+        <h5>Edit</h5>
+        <h4 name="drawing-tools">Drawing tools</h4>
+        <p>This section is located on the left of the page and offer all the options for drawing.</p>
+        <div>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 512 512" xml:space="preserve">
+            <polygon class="fill" points="429.742,319.31 82.489,0 82.258,471.744 187.633,370.918 249.523,512 346.083,469.642 284.193,328.56"/>
+            </svg>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 493.638 493.638" xml:space="preserve">
+            <polygon class="fill" fill="#020202" points="427.092,118.538 99.89,445.738 90.239,436.086 417.439,108.885 358.708,50.154
+            31.376,377.481 116.022,462.13 443.351,134.798"/>
+            <path class="fill" file="#020202" d="M492.627,85.523c-10.064,23.448-43.053,43.053-43.053,43.053l-84.646-84.644
+            c0,0,27.29-33.513,43.054-43.053S502.692,62.074,492.627,85.523z"/>
+            <polygon class="fill" fill="#020202" points="110.059,468.088 0.127,493.638 25.413,383.447"/>
+            </svg>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 511.998 511.998" xml:space="preserve">
+            <line class="stroke" fill="none" stroke="#000" stroke-width="70" stroke-opacity="null" fill-opacity="null" x1="0" y1="0"
+            x2="511.998" y2="511.998" stroke-linejoin="null" stroke-linecap="null" stroke-dasharray="none"></line>
+          </svg>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 64 48" xml:space="preserve">
+            <rect class="fill" width="64" height="48"/>
+            </svg>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 512 300" xml:space="preserve">
+            <ellipse class="fill" fill="none" stroke="none" cx="256" cy="150" rx="256" ry="150"></ellipse>
+            </svg>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 900.5 900.5" style="enable-background:new 0 0 900.5 900.5;" xml:space="preserve">
+              <g>
+                <path class="fill" fill="#020202" d="M176.415,880.5c0,11.046,8.954,20,20,20h507.67c11.046,0,20-8.954,20-20V232.487h-547.67V880.5L176.415,880.5z
+                M562.75,342.766h75v436.029h-75V342.766z M412.75,342.766h75v436.029h-75V342.766z M262.75,342.766h75v436.029h-75V342.766z"/>
+                <path class="fill" fill="#020202" d="M618.825,91.911V20c0-11.046-8.954-20-20-20h-297.15c-11.046,0-20,8.954-20,20v71.911v12.5v12.5H141.874
+                c-11.046,0-20,8.954-20,20v50.576c0,11.045,8.954,20,20,20h34.541h547.67h34.541c11.046,0,20-8.955,20-20v-50.576
+                c0-11.046-8.954-20-20-20H618.825v-12.5V91.911z M543.825,112.799h-187.15v-8.389v-12.5V75h187.15v16.911v12.5V112.799z"/>
+              </g>
+            </svg>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+              viewBox="0 0 96 96" xml:space="preserve">
+              <rect class="fill-color" fill="#f00" width="64" height="64"/>
+              <g >
+                <rect class="stroke-color" stroke="#fff" stroke-width="1" fill="#0f0" x="32" y="32" width="64" height="64"/>
+                <rect fill="#fff" stroke-width="1" x="52" y="52" width="24" height="24"/>
+              </g>
+              <use xlink:href="#fill-color"/>
+            </svg>
+        </div>
+        <h5>File</h5>
+        <h4 name="editor">Editor</h4>
+        <h4 name="shapes">Shapes section</h4>
+
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
